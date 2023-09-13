01720bcb672832376c345b9847af1885579f2561
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-09-13 20:12:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Wrapper from './components/Wrapper'
+
+function App() {
+  return (
+    <div>
+        <Wrapper />
+    </div>
+  ) 
+}
+
+export default App;
