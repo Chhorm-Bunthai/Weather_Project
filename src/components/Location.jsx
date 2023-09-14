9e05316ac1e6558197bf3b32b1ae93a9cc77a3c5
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-09-14 15:42:40 +0700

    Done Location component for issue1 and already and icon file for the projects

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function Location() {
+function Location({onLocation}) {
+
+  const nameOfCity = onLocation && onLocation.name ? onLocation.name : null;
+  const CountryName = onLocation && onLocation.sys && onLocation.sys.country ? onLocation.sys.country : null;
+
+  // console.log(onLocation)
   return (
-    <div>Location</div>
+    <div className='location'>
+        {onLocation ? <h1 className='city'>{nameOfCity}</h1>: null}
+        {onLocation ? <p>{CountryName}</p>: null}
+    </div>
   )
 }
 
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,10 +1,57 @@
+import { useState, useEffect } from 'react';
 import React from 'react'
+import Location from './Location';
+import Time from './Time';
+import SearchBar from './SearchBar';
+import ShowWeather from './ShowWeather';
+import axios from 'axios';
 
 function Wrapper() {
+
+  // fetching current day
+  const [value, setValue] = useState([]);
+
+  function  fetchData() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const currUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1ed49b57432b841036895e62c128d585&units=metric`;
+        try {
+          const res = await axios(currUrl);
+          setValue(res.data);
+          console.log(res.data);
+        } catch (error) {
+          console.error("Error fetching weather data:", error);
+        }
+      });
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-        Hello
-    </div>
+    <main>
+    <section className="top">
+      <div className="container">
+        <div className="row mx-auto justify-content-center align-items-center">
+          <div className="col-md g-0 ">
+            <Time />
+          </div>
+          <div className="col-md g-0">
+            <Location onLocation={value}/>
+          </div>
+        </div>
+      </div>
+      <SearchBar />
+    </section>
+    <footer>
+      <ShowWeather/>
+    </footer>
+  </main>
   )
 }
 
